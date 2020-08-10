7bf9d3e2d1c954a46ec6a6e1c4c13956e141945c
Rael Gugelmin Cunha <rael.gc@gmail.com>
2020-08-10 09:08:33 -0300

    Removing hardcoded zoom

--- zenhub_viewer/webengineview.py
+++ zenhub_viewer/webengineview.py
@@ -6,7 +6,6 @@ class WebEngineView(QWebEngineView):
   def __init__(self, parent = None):
     super().__init__(parent)
     self.setPage(WebEnginePage(self))
-    self.setZoomFactor(1.3)
     settings = self.settings()
     settings.setAttribute(QWebEngineSettings.JavascriptEnabled, True)
     settings.setAttribute(QWebEngineSettings.JavascriptCanOpenWindows, True)
